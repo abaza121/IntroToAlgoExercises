8bcaa68c1c942c256e129efac3640066f2e553b6
Mahmoud Abaza <mahmoud.abaza@live.com>
2020-10-16 15:26:39 +0200

    Add project files.

---
+++ IntroToAlgoExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroToAlgoExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
